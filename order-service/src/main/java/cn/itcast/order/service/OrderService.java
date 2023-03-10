27671ffc0c4ec4bc0ba003d0b297c00bc400d540
dmsWide <dmswide@163.com>
2023-03-10 09:54:55 +0800

    使用feign发送请求调用服务

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,10 +1,10 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -14,6 +14,20 @@ public class OrderService {
     @Resource
     private OrderMapper orderMapper;
     @Resource
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        //2、使用feign调用findById方法即可
+        User user = userClient.findById(order.getUserId());
+        //3、封装user
+        order.setUser(user);
+        //4、返回
+        return order;
+    }
+
+    /*@Resource
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -28,5 +42,5 @@ public class OrderService {
         //将user填入order中
         order.setUser(user);
         return order;
-    }
+    }*/
 }
